7bd14cd61f0bfd7772bdd8f54bf16189f24b4929
8180e <emirceylan31@hotmail.com>
2025-12-10 22:42:09 +0100

    feat(categories): add getting category by ID

--- src/modules/categories.ts
+++ src/modules/categories.ts
@@ -6,6 +6,16 @@ const CategoriesSchema = createResponseSchema(
   z.array(z.object({ id: z.number(), name: z.string(), thumbnail: z.string() }))
 );
 
+const CategorySchema = createResponseSchema(
+  z.object({
+    id: z.number(),
+    name: z.string(),
+    tags: z.array(z.string()),
+    thumbnail: z.string(),
+    viewer_count: z.number(),
+  })
+);
+
 export class CategoriesAPI {
   constructor(private readonly options: AppClientOptions | UserClientOptions) {}
 
@@ -19,5 +29,13 @@ export class CategoriesAPI {
       { headers: { Authorization: `Bearer ${this.options.accessToken}` } }
     );
     return formatData(CategoriesSchema, await res.json()).data;
+  }
+
+  async getCategoryById(categoryId: number) {
+    const res = await fetch(
+      `https://api.kick.com/public/v1/categories/${categoryId}`,
+      { headers: { Authorization: `Bearer ${this.options.accessToken}` } }
+    );
+    return formatData(CategorySchema, await res.json()).data;
   }
 }
