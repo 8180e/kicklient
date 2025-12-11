b991c9500abe3de582c6bf18812b8c4b2736e0c5
8180e <emirceylan31@hotmail.com>
2025-12-11 10:20:39 +0100

    refactor(categories): extend from base API client

--- src/modules/categories.ts
+++ src/modules/categories.ts
@@ -1,41 +1,35 @@
 import z from "zod";
-import type { AppClientOptions, UserClientOptions } from "../client.js";
-import { createResponseSchema, formatData } from "../utils.js";
+import { parseSchema } from "../utils.js";
+import { KickAPIClient } from "../api-client.js";
 
-const CategoriesSchema = createResponseSchema(
-  z.array(z.object({ id: z.number(), name: z.string(), thumbnail: z.string() }))
+const CategoriesSchema = z.array(
+  z.object({ id: z.number(), name: z.string(), thumbnail: z.string() })
 );
 
-const CategorySchema = createResponseSchema(
-  z.object({
-    id: z.number(),
-    name: z.string(),
-    tags: z.array(z.string()),
-    thumbnail: z.string(),
-    viewer_count: z.number(),
-  })
-);
+const CategorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  tags: z.array(z.string()),
+  thumbnail: z.string(),
+  viewerCount: z.number(),
+});
 
-export class CategoriesAPI {
-  constructor(private readonly options: AppClientOptions | UserClientOptions) {}
-
+export class CategoriesAPI extends KickAPIClient {
   async getCategories(q: string, page?: number) {
     const params = new URLSearchParams({ q });
     if (page) {
       params.append("page", page.toString());
     }
-    const res = await fetch(
-      `https://api.kick.com/public/v1/categories?${params}`,
-      { headers: { Authorization: `Bearer ${this.options.accessToken}` } }
+    return parseSchema(
+      CategoriesSchema,
+      await this.get(`/categories?${params}`)
     );
-    return formatData(CategoriesSchema, await res.json()).data;
   }
 
   async getCategoryById(categoryId: number) {
-    const res = await fetch(
-      `https://api.kick.com/public/v1/categories/${categoryId}`,
-      { headers: { Authorization: `Bearer ${this.options.accessToken}` } }
+    return parseSchema(
+      CategorySchema,
+      await this.get(`/categories/${categoryId}`)
     );
-    return formatData(CategorySchema, await res.json()).data;
   }
 }
