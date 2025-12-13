d11c5070187ca2cbcd95a151d78ac5872ddb251e
8180e <emirceylan31@hotmail.com>
2025-12-13 21:45:30 +0100

    feat(chat): add posting chat message

--- src/modules/chat.ts
+++ src/modules/chat.ts
@@ -1,3 +1,31 @@
+import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 
-export class ChatAPI extends KickAPIClient {}
+const PostMessageOptionsSchema = z.intersection(
+  z.object({
+    content: z.string().max(500),
+    replyToMessageId: z.uuid().optional(),
+  }),
+  z.union([
+    z.object({ type: z.literal("user"), broadcasterUserId: z.int() }),
+    z.object({ type: z.literal("bot") }),
+  ])
+);
+
+const PostMessageResponseSchema = z.object({
+  is_sent: z.boolean(),
+  message_id: z.string(),
+});
+
+export class ChatAPI extends KickAPIClient {
+  postChatMessage(options: z.infer<typeof PostMessageOptionsSchema>) {
+    return this.postWithResponseData(
+      "/chat",
+      options,
+      PostMessageOptionsSchema,
+      PostMessageResponseSchema,
+      true,
+      ["chat:write"]
+    );
+  }
+}
