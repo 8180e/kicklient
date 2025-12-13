0eac82522490d1b72fd85b57599fd87da2dc12f1
8180e <emirceylan31@hotmail.com>
2025-12-13 16:07:35 +0100

    feat(channel-rewards): add getting channel rewards

--- src/modules/channel-rewards.ts
+++ src/modules/channel-rewards.ts
@@ -1,3 +1,24 @@
+import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 
-export class ChannelRewardsAPI extends KickAPIClient {}
+const ChannelRewardsSchema = z.array(
+  z.object({
+    background_color: z.string(),
+    cost: z.number(),
+    description: z.string(),
+    id: z.string(),
+    is_enabled: z.boolean(),
+    is_paused: z.boolean(),
+    is_user_input_required: z.boolean(),
+    should_redemptions_skip_request_queue: z.boolean(),
+    title: z.string(),
+  })
+);
+
+export class ChannelRewardsAPI extends KickAPIClient {
+  getChannelRewards() {
+    return this.get("/channels/rewards", ChannelRewardsSchema, true, [
+      "channel:rewards:write",
+    ]);
+  }
+}
