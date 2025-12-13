067293543efff56938ba07a99245093e047c1673
8180e <emirceylan31@hotmail.com>
2025-12-13 16:30:47 +0100

    feat(channel-rewards): add creating channel reward

--- src/modules/channel-rewards.ts
+++ src/modules/channel-rewards.ts
@@ -1,24 +1,50 @@
 import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 
-const ChannelRewardsSchema = z.array(
-  z.object({
-    background_color: z.string(),
-    cost: z.number(),
-    description: z.string(),
-    id: z.string(),
-    is_enabled: z.boolean(),
-    is_paused: z.boolean(),
-    is_user_input_required: z.boolean(),
-    should_redemptions_skip_request_queue: z.boolean(),
-    title: z.string(),
-  })
-);
+const ChannelRewardSchema = z.object({
+  background_color: z.string(),
+  cost: z.number(),
+  description: z.string(),
+  id: z.string(),
+  is_enabled: z.boolean(),
+  is_paused: z.boolean(),
+  is_user_input_required: z.boolean(),
+  should_redemptions_skip_request_queue: z.boolean(),
+  title: z.string(),
+});
+
+const ChannelRewardsSchema = z.array(ChannelRewardSchema);
+
+const ChannelRewardOptionsSchema = z.object({
+  backgroundColor: z
+    .string()
+    .regex(/^#(?:[0-9a-fA-F]{3,4}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$/, {
+      error: "Invalid hex color format",
+    })
+    .optional(),
+  cost: z.int().min(1),
+  description: z.string().max(200).optional(),
+  isEnabled: z.boolean().optional(),
+  isUserInputRequired: z.boolean().optional(),
+  shouldRedemptionsSkipRequestQueue: z.boolean().optional(),
+  title: z.string().max(50),
+});
 
 export class ChannelRewardsAPI extends KickAPIClient {
   getChannelRewards() {
     return this.get("/channels/rewards", ChannelRewardsSchema, true, [
       "channel:rewards:write",
     ]);
+  }
+
+  createChannelReward(options: z.infer<typeof ChannelRewardOptionsSchema>) {
+    return this.postWithResponseData(
+      "/channels/rewards",
+      options,
+      ChannelRewardOptionsSchema,
+      ChannelRewardSchema,
+      true,
+      ["channel:rewards:write"]
+    );
   }
 }
