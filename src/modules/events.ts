3f5b7e3d446d37e828e25edba866c4f8dde008d9
8180e <emirceylan31@hotmail.com>
2025-12-20 21:30:34 +0100

    feat(events): add getting events subscriptions

--- src/modules/events.ts
+++ src/modules/events.ts
@@ -1,3 +1,42 @@
+import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 
-export class EventsAPI extends KickAPIClient {}
+const EventSubscriptionSchema = z.array(
+  z.object({
+    app_id: z.string(),
+    broadcaster_user_id: z.number(),
+    created_at: z.string(),
+    event: z.enum([
+      "chat.message.sent",
+      "channel.followed",
+      "channel.subscription.renewal",
+      "channel.subscription.gifts",
+      "channel.subscription.new",
+      "channel.reward.redemption.updated",
+      "livestream.status.updated",
+      "livestream.metadata.updated",
+      "moderation.banned",
+      "kicks.gifted",
+    ]),
+    id: z.string(),
+    method: z.string(),
+    updated_at: z.string(),
+    version: z.number(),
+  })
+);
+
+export class EventsAPI extends KickAPIClient {
+  async getEventsSubscriptions(broadcasterUserId?: number) {
+    const params = new URLSearchParams();
+    if (broadcasterUserId) {
+      params.append("broadcaster_user_id", broadcasterUserId.toString());
+    }
+    return (
+      await this.get(`/events/subscriptions?${params}`, EventSubscriptionSchema)
+    ).map(({ createdAt, updatedAt, ...subscription }) => ({
+      createdAt: new Date(createdAt),
+      updatedAt: new Date(updatedAt),
+      ...subscription,
+    }));
+  }
+}
