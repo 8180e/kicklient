bf56b36b391c271bfc92b93844efc970e6629139
8180e <emirceylan31@hotmail.com>
2026-03-13 22:14:36 +0100

    feat(categories): use generator function to fetch categories

--- src/modules/categories.ts
+++ src/modules/categories.ts
@@ -1,28 +1,57 @@
 import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 
-const CategoriesSchema = z.array(
-  z.object({ id: z.number(), name: z.string(), thumbnail: z.string() })
-);
+interface GetCategoriesOptions {
+  limit?: number;
+}
 
-const CategorySchema = z.object({
-  id: z.number(),
-  name: z.string(),
-  tags: z.array(z.string()),
-  thumbnail: z.string(),
-  viewer_count: z.number(),
+interface GetCategoriesParams extends GetCategoriesOptions {
+  names?: string[];
+  tags?: string[];
+}
+
+interface GetCategoriesByIdsParams extends GetCategoriesOptions {
+  ids: number[];
+}
+
+const CategoriesSchema = z.object({
+  data: z.array(
+    z.object({ id: z.number(), name: z.string(), thumbnail: z.string() }),
+  ),
+  pagination: z.object({ next_cursor: z.string() }),
 });
 
 export class CategoriesAPI extends KickAPIClient {
-  getCategories(q: string, page?: number) {
-    const params = new URLSearchParams({ q });
-    if (page) {
-      params.append("page", page.toString());
-    }
-    return this.get(`/categories?${params}`, CategoriesSchema);
+  private makeCategoriesRequest(params: URLSearchParams) {
+    return this.get(`/v2/categories?${params}`, CategoriesSchema);
   }
 
-  getCategoryById(categoryId: number) {
-    return this.get(`/categories/${categoryId}`, CategorySchema);
+  private async *getCategoriesData(params: URLSearchParams, limit?: number) {
+    if (limit) params.set("limit", limit.toString());
+
+    let response = await this.makeCategoriesRequest(params);
+
+    yield response.data;
+
+    while (response.pagination.nextCursor) {
+      response = await this.makeCategoriesRequest(
+        new URLSearchParams({ cursor: response.pagination.nextCursor }),
+      );
+
+      yield response.data;
+    }
+  }
+
+  getCategories({ names = [], tags = [], limit }: GetCategoriesParams) {
+    const params = new URLSearchParams();
+    for (const name of names) params.append("name", name);
+    for (const tag of tags) params.append("tag", tag);
+    return this.getCategoriesData(params, limit);
+  }
+
+  getCategoriesByIds({ ids, limit }: GetCategoriesByIdsParams) {
+    const params = new URLSearchParams();
+    for (const id of ids) params.append("id", id.toString());
+    return this.getCategoriesData(params, limit);
   }
 }
