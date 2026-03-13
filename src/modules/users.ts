30f883d72993975a10a486f77a2b3ae82d456510
8180e <emirceylan31@hotmail.com>
2026-03-13 22:46:07 +0100

    chore(users): remove scope checks

--- src/modules/users.ts
+++ src/modules/users.ts
@@ -1,43 +1,42 @@
 import z from "zod";
-import { KickAPIClient, UserKickAPIClient } from "../api-client.js";
-import { KickAPIError } from "../errors.js";
+import { KickAPIClient } from "../api-client.js";
+import { KickError } from "../errors.js";
 
-const UsersSchema = z.array(
-  z.object({
-    email: z.string(),
-    name: z.string(),
-    profile_picture: z.string(),
-    user_id: z.number(),
-  })
-);
+interface GetUsersByIdsParams {
+  ids: number[];
+}
+
+const UsersSchema = z.object({
+  data: z.array(
+    z.object({
+      email: z.string(),
+      name: z.string(),
+      profile_picture: z.string(),
+      user_id: z.number(),
+    }),
+  ),
+});
 
 export class UsersAPI extends KickAPIClient {
-  getUsersById(...ids: number[]) {
+  protected async getUsersData(params?: URLSearchParams) {
+    return (await this.get(`/v1/users?${params || ""}`, UsersSchema)).data;
+  }
+
+  async getUsersByIds({ ids }: GetUsersByIdsParams) {
     const params = new URLSearchParams();
-    for (const id of ids) {
-      params.append("id", id.toString());
-    }
-    return this.get(`/users?${params}`, UsersSchema);
+    for (const id of ids) params.append("id", id.toString());
+    return this.getUsersData(params);
   }
 }
 
-export class UserUsersAPI extends UserKickAPIClient {
-  private readonly api = new UsersAPI(this.token, this.onTokensRefreshed);
-
+export class UserUsersAPI extends UsersAPI {
   async getAuthenticatedUser() {
-    this.requireScopes("user:read");
-    const user = (await this.get("/users", UsersSchema))[0];
+    const user = (await this.getUsersData())[0];
     if (!user) {
-      throw new KickAPIError({
-        message:
-          "Expected the API to return the authenticated user, but got no user",
-      });
+      throw new KickError(
+        "Expected the API to return the authenticated user, but got no user",
+      );
     }
     return user;
-  }
-
-  getUsersById(...ids: number[]) {
-    this.requireScopes("user:read");
-    return this.api.getUsersById(...ids);
   }
 }
