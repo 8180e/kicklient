302225c1f318c6d38867d2f84c464fb739d5767b
8180e <emirceylan31@hotmail.com>
2026-03-14 16:22:37 +0100

    feat(chat): add replying to chat from chat data

--- src/modules/chat.ts
+++ src/modules/chat.ts
@@ -2,10 +2,19 @@ import z from "zod";
 import { KickAPIClient } from "../api-client.js";
 import decamelizeKeys from "decamelize-keys";
 
+interface PostChatMessageAsBotParams {
+  content: string;
+  replyToMessageId?: string;
+}
+
+interface PostChatMessageAsUserParams extends PostChatMessageAsBotParams {
+  broadcasterUserId: number;
+}
+
 type PostChatMessageParams = (
   | { type: "user"; broadcasterUserId: number }
   | { type: "bot" }
-) & { content: string; replyToMessageId: string };
+) & { content: string; replyToMessageId?: string };
 
 const PostMessageResponseSchema = z.object({
   is_sent: z.boolean(),
@@ -13,10 +22,35 @@ const PostMessageResponseSchema = z.object({
 });
 
 export class ChatAPI extends KickAPIClient {
-  async postChatMessage(params: PostChatMessageParams) {
+  private async postChatMessage(params: PostChatMessageParams) {
     const res = await this.post("/v1/chat", decamelizeKeys(params));
     const { data } = await res.getData(PostMessageResponseSchema);
-    return { ...data, delete: () => this.deleteChatMessage(data.messageId) };
+    return {
+      ...data,
+      delete: () => this.deleteChatMessage(data.messageId),
+      replyAsBot: (
+        params: Omit<PostChatMessageAsBotParams, "replyToMessageId">,
+      ) =>
+        this.postChatMessageAsBot({
+          ...params,
+          replyToMessageId: data.messageId,
+        }),
+      replyAsUser: (
+        params: Omit<PostChatMessageAsUserParams, "replyToMessageId">,
+      ) =>
+        this.postChatMessageAsUser({
+          ...params,
+          replyToMessageId: data.messageId,
+        }),
+    };
+  }
+
+  postChatMessageAsBot(params: PostChatMessageAsBotParams) {
+    return this.postChatMessage({ ...params, type: "bot" });
+  }
+
+  postChatMessageAsUser(params: PostChatMessageAsUserParams) {
+    return this.postChatMessage({ ...params, type: "user" });
   }
 
   deleteChatMessage(messageId: string) {
